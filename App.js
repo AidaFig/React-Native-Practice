ceb57d97d0da0549f1995505eb42de6953768efa
AidaFig <aidafigueiredo8@gmail.com>
2022-01-29 22:33:27 -0300

    Adding the Home component in the Ap.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Home} from './src/pages/Home';
 
 export default function App() {
-  return <Text>Hello World</Text>;
+  return <Home />;
 }
