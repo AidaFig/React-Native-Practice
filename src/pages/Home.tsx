ec61516e7caf2229d985c82f40cb2915ffea6ce9
AidaFig <aidafigueiredo8@gmail.com>
2022-02-01 22:28:56 -0300

    adding types

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,13 +3,22 @@ import {Button} from '../components/Button';
 import {SkillCard} from '../components/SkillCard';
 import {View, Text, StyleSheet, TextInput, FlatList} from 'react-native';
 
+interface skillData {
+  id: string;
+  name: string;
+}
+
 export function Home() {
   const [newSkill, setNewSkill] = useState('');
-  const [mySkills, setMySkills] = useState([]);
+  const [mySkills, setMySkills] = useState<skillData[]>([]);
   const [greeting, setGreeting] = useState('');
 
   function handleAddNewSkill() {
-    setMySkills(oldState => [...oldState, newSkill]);
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill,
+    }
+    setMySkills(oldState => [...oldState, data]);
   }
 
   useEffect(() => {
@@ -49,8 +58,8 @@ export function Home() {
       </Text>
       <FlatList
         data={mySkills}
-        keyExtractor={item => item}
-        renderItem={({item}) => <SkillCard skill={item} />}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => <SkillCard skill={item.name} />}
       />
     </View>
   );
